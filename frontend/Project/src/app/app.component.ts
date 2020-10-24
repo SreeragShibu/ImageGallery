0281c683b4c82944e22f00906dde9cc15f1f9be2
Sreerag Shibu <sreerag@lycaeum.in>
2020-10-24 19:55:40 +0530

    added funcitonality in frontend to display images

--- frontend/Project/src/app/app.component.ts
+++ frontend/Project/src/app/app.component.ts
@@ -11,13 +11,14 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 })
 export class AppComponent {
   name : string;
+  description : string;
   image: File;
 
   // constructor( private _freeApiService: freeApiService){
   // } 
   images : Image[];
 
-  constructor( private http:HttpClient){}
+  constructor( private http:HttpClient, private _freeApiService: freeApiService){}
   onNameChanged(event: any){
     this.name = event.target.value;
 
@@ -26,23 +27,29 @@ export class AppComponent {
     this.image = event.target.files[0];
   }
 
+  onDescriptionChanged(event:any){
+    this.description = event.target.value;
+  }
+
   newBook(){
     const uploadData = new FormData();
     uploadData.append('name', this.name);
+    uploadData.append('description', this.description);
     uploadData.append('image',this.image, this.image.name);
     this.http.post('http://127.0.0.1:8000/images/', uploadData).subscribe(
       data => console.log(data),
       error => console.log(error)
     )
   }
+  
 
-  // ngOnInit(){
-  //   this._freeApiService.getimages()
-  //   .subscribe(
-  //     data=>
-  //     {
-  //       this.images = data;
-  //     }
-  //   );
-  // }
+  ngOnInit(){
+    this._freeApiService.getimages()
+    .subscribe(
+      data=>
+      {
+        this.images = data;
+      }
+    );
+  }
 }
